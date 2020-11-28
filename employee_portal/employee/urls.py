90954e7945dbacd43f997a1bc0ceb36fc3830a0c
ishita-m <isha130695@gmail.com>
2020-11-28 16:49:14 +0530

    added update and delete employee api, also added api to retrieve employee details based on id

--- employee_portal/employee/urls.py
+++ employee_portal/employee/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
 	path('', views.index, name = "list"),
 	path('enroll/employee', views.enroll_employee, name = "enroll"),
+	path('update_employee_details/<str:pk>/', views.update_employee_details, name = "update_emp"),
+	path('delete/<str:pk>/', views.delete_employee, name = "delete"),
+	path('employee_data/<str:pk>/', views.get_employee, name = "employee_data"),
 ]  
