e404a03a3495d23e8d63319873fef22e1eb2f47e
ishita-m <isha130695@gmail.com>
2020-11-30 12:45:05 +0530

    corrected the url from employee_data/<id> to employee/<id>

--- employee_portal/employee/urls.py
+++ employee_portal/employee/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	path('enroll/employee', views.enroll_employee, name = "enroll"),
 	path('update_employee_details/<str:pk>/', views.update_employee_details	, name = "update_emp"),
 	path('delete/<str:pk>/', views.delete_employee, name = "delete"),
-	path('employee_data/<str:pk>/', views.get_employee, name = "employee_data"),
+	path('employee/<str:pk>/', views.get_employee, name = "employee_data"),
 	path('order_by/<str:attr>/', views.order_by, name = "order_by"),
 	path('search_results', views.search_employee, name = "search_employee"),
 ]  
